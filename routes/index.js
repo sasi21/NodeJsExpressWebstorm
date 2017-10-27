a9f4d76e57d7156e71e6570a2ea785a503b2747b
sasivarna <Netaji_23>
2017-10-26 17:30:12 -0700

    final commit mongo db added

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,34 @@
-
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
 var express = require('express');
 var router = express.Router();
-var app = router;
-var pg = require('pg');
-app.get('/db', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.render('pages/db', {results: result.rows} ); }
+var mongodb = require('mongodb');
+
+router.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect('mongodb://heroku_82mvms7r:i9srgbo7q1dfeod09ue7fseg5q@ds235785.mlab.com:35785/heroku_82mvms7r', function(err, db) {
+        if(err) throw err;
+        //get collection of routes
+        var Routes = db.collection('Routes');
+        //get all Routes with frequency >=1
+        Routes.find({ frequency : { $gte: 1 } }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+            response.render('pages/mongodb', {results: docs});
         });
-    });
-});
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+});//end app.get
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Exercise N1' });
 });
 
 module.exports = router;
+
+
